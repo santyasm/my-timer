80d960ffaa875272ea1b4cba063df08c1433f761
Yasmin Santana <94482549+santyasm@users.noreply.github.com>
2022-03-07 19:56:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function criaHoraDoSegundos(seconds) {
+    const data = new Date(seconds * 1000);
+    return data.toLocaleTimeString("pt-BR",{
+        timeZone: "GMT"
+    });
+}
+console.log(criaHoraDoSegundos(10))
+
+
+
+const relogio = document.querySelector(".relogio");
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const zerar = document.querySelector(".zerar");
+let segundos = 0;
+let timer; 
+
+function iniciaRelogio() {
+     timer = setInterval(function () {
+        segundos++
+        relogio.innerHTML = criaHoraDoSegundos(segundos)
+    }, 1000);
+}
+
+iniciar.addEventListener("click", function (envent) {
+    relogio.classList.remove("pausado");
+    clearInterval(timer)
+    iniciaRelogio();
+});
+
+pausar.addEventListener("click", function (envent) {
+    relogio.classList.add("pausado")
+    clearInterval(timer);
+});
+
+zerar.addEventListener("click", function (envent) {
+    clearInterval(timer);
+    relogio.innerHTML = "00:00:00"
+    segundos = 0;
+});
